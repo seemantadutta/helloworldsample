5168df2ecf8543ad0a01f1d189900f47ec0c2324
Seemanta Dutta <seemanta@gmail.com>
2012-05-31 17:45:31 -0700

    added hello.c file

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 0;
+    printf("hello, world\n");
+
+    // Useless loop -- Thomas, you can change it to test for conflicts
+    for (; i < 100; i++)
+    {
+        printf("I usually don't create a open source repo in Github, but when I do, it is a hello world program\n");
+    }
+
+
+    exit(0);
+}
+
